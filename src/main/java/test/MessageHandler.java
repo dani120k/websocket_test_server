07538fa83be14bcd91653b0b39ac3369b7e273f1
forol <forolymp120@gmail.com>
2019-05-17 11:11:09 +0300

    -

--- src/main/java/test/MessageHandler.java
+++ src/main/java/test/MessageHandler.java
@@ -9,68 +9,52 @@ import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketChunkedInput;
 import io.netty.handler.stream.ChunkedStream;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class MessageHandler {
     private String text;
     private ChannelHandlerContext ctx;
+    private byte[] response;
 
     public MessageHandler(ChannelHandlerContext ctx, String text){
         this.ctx = ctx;
         this.text = text;
     }
 
-    private static byte[] readFileToByteArray(File file){
+    private static byte[] readFileToByteArray(File file) throws Exception{
         FileInputStream fis = null;
-        // Creating a byte array using the length of the file
-        // file.length returns long which is cast to int
+
         byte[] bArray = new byte[(int) file.length()];
-        try{
             fis = new FileInputStream(file);
             fis.read(bArray);
             fis.close();
-
-        }catch(IOException ioExp){
-            ioExp.printStackTrace();
-        }
         return bArray;
     }
 
     public void handle(){
-        File file = new File("test.jpeg");
-        byte[] bArray = readFileToByteArray(file);
+        try {
+            File file = new File(this.text);
+            byte[] bArray = readFileToByteArray(file);
+            sendMessage(bArray);
+        }
+        catch (Exception ex){
+            sendBadAnswer(getErrorBlob());
+        }
 
-        sendMessage(bArray);
+
+
+    }
+
+
+    public void sendBadAnswer(byte[] msg){
+        ctx.writeAndFlush(new BinaryWebSocketFrame(Unpooled.wrappedBuffer(msg)));
+    }
+
+    public byte[] getErrorBlob() {
+        return "$ERROR".getBytes();
     }
 
     public void sendMessage(byte[] msg) {
-        ChunkedStream stream = new ChunkedStream(new ByteArrayInputStream(msg));
-        ChannelFuture sendFuture;
-        sendFuture = ctx.write(new WebSocketChunkedInput(stream), ctx.newProgressivePromise());
-        sendFuture.addListener(new ChannelProgressiveFutureListener() {
-            @Override
-            public void operationProgressed(ChannelProgressiveFuture future, long progress, long total) {
-                if (total < 0) { // total unknown
-                    System.err.println(future.channel() + " Transfer progress: " + progress);
-                } else {
-                    System.err.println(future.channel() + " Transfer progress: " + progress + " / " + total);
-                }
-            }
-
-            @Override
-            public void operationComplete(ChannelProgressiveFuture future) {
-                System.err.println(future.channel() + " Transfer complete.");
-            }
-        });
-
-        ctx.flush();
-
-        if (!sendFuture.isSuccess()) {
-            System.out.println("Send failed: " + sendFuture.cause());
-        }
         ctx.writeAndFlush(new BinaryWebSocketFrame(Unpooled.wrappedBuffer(msg)));
     }
 }
